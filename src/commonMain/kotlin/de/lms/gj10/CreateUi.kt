6dae8a15c06e4426804544f266658db1064b6655
Steve Gotzel <steve.gotzel@3m5.de>
2023-11-11 17:38:10 +0100

    update CreateUI

--- src/commonMain/kotlin/de/lms/gj10/CreateUi.kt
+++ src/commonMain/kotlin/de/lms/gj10/CreateUi.kt
@@ -1,26 +1,36 @@
 package de.lms.gj10
 
-import korlibs.image.bitmap.*
 import korlibs.image.color.*
 import korlibs.image.font.*
-import korlibs.image.format.*
 import korlibs.image.text.*
-import korlibs.io.file.std.*
-import korlibs.korge.ui.uiButton
+import korlibs.korge.ui.*
 import korlibs.korge.view.*
 import korlibs.math.geom.*
-
+enum class UiBtnType {
+    BuildFactory,
+}
 class GameUi(
     // private val scoreTextField: Text,
     private val container : SContainer,
-    private val onBuildBtnPress : () -> Unit,
+    private val onBuildBtnPress : (UiBtnType) -> Unit,
 ) {
-    private var textMoney: TextBlock?=null
+    private val textMoney: TextBlock
+    private val myBtn: UIButton
     fun onMoneyChanged(newMoney: Long) {
 //        textMoney.plainText = 'test'
         // TODO
     }
-    suspend fun createUi() {
+    fun onBtnTypeChange(btnType: UiBtnType) {
+//        myBtn.bgColorOut = ()
+        updateBtnActive(btnType == UiBtnType.BuildFactory, myBtn)
+//        color = if UiBtnType = mytype ? RED : BLUE
+    }
+
+    private fun updateBtnActive(active: Boolean, btn: UIButton) {
+//        if ()
+    }
+
+    init {
         textMoney = container.textBlock(
             RichTextData.fromHTML(
                 "hello <b>world</b>, <font color=red>this</font> is a long text that won't fit!",
@@ -29,24 +39,21 @@ class GameUi(
             size = Size(100f, 48f)
         )
 
-        container.generateButton("a")
-
+        myBtn = container.generateButton("a")
     }
-}
+
+    private fun SContainer.generateButton(hotkey: String): UIButton {
+        val btnSize = 500
+        val btnPosX = 20
+        val btnPosY = 20
 
 
-private suspend fun SContainer.generateButton(hotkey: String) {
-    val btnSize = 500
-    val btnPosX = 20
-    val btnPosY = 20
-
-
-    // Create button
-    uiButton() {
-        bgColorOut = Colors.TRANSPARENT
-        bgColorDisabled = Colors.TRANSPARENT
-        bgColorOver = Colors.TRANSPARENT
-        bgColorSelected = Colors.TRANSPARENT
+        // Initial Create button
+        return uiButton() {
+            bgColorOut = Colors.TRANSPARENT
+            bgColorDisabled = Colors.TRANSPARENT
+            bgColorOver = Colors.TRANSPARENT
+            bgColorSelected = Colors.TRANSPARENT
 //        colorMul  = Colors.TRANSPARENT
 //        color = Colors.RED
 
@@ -54,28 +61,32 @@ private suspend fun SContainer.generateButton(hotkey: String) {
 //        bgColorDisabled = Colors.TRANSPARENT_WHITE
 //        bgColorOver = Colors.TRANSPARENT_WHITE
 //        bgColorSelected = Colors.TRANSPARENT_WHITE
-        // Set the background image
-        size(btnSize, btnSize)
-        background.radius = RectCorners(btnSize / 12, btnSize / 12, btnSize / 4, btnSize / 12)
-        position(10, 10)
-        onPress { println("TAPPED ON 3") }
-
-        image(gameResources.images.glassPanel_cornerBR_Bitmap) {
-            smoothing = false
+            // Set the background image
             size(btnSize, btnSize)
-        }
+            background.radius = RectCorners(btnSize / 12, btnSize / 12, btnSize / 4, btnSize / 12)
+            position(10, 10)
+            onPress { println("TAPPED ON 3") }
 
-        // Hotkey Image
-        image(gameResources.images.hotkeyBitmap) {
-            smoothing = false
-            position(5, 5)
-        }
+            image(gameResources.images.glassPanel_cornerBR_Bitmap) {
+                smoothing = false
+                size(btnSize, btnSize)
+            }
 
-        // Main Image
-        image(gameResources.images.iconBitmap) {
-            smoothing = false
-            position(0, height - height / 4)
-            size(btnSize / 2, btnSize / 2)
+            // Hotkey Image
+            image(gameResources.images.hotkeyBitmap) {
+                smoothing = false
+                position(5, 5)
+            }
+
+            // Main Image
+            image(gameResources.images.iconBitmap) {
+                smoothing = false
+                position(0, height - height / 4)
+                size(btnSize / 2, btnSize / 2)
+            }
         }
     }
+
 }
+
+
