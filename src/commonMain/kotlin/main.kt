93329c416069c00063e46fc1414f2659c0bce6f0
LostMekka <LostMekka@users.noreply.github.com>
2023-11-11 12:00:46 +0100

    auto format main kt

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -10,26 +10,25 @@ import korlibs.math.geom.*
 import korlibs.math.interpolation.*
 
 suspend fun main() = Korge(windowSize = Size(512, 512), backgroundColor = Colors["#2b2b2b"]) {
-	val sceneContainer = sceneContainer()
-
-	sceneContainer.changeTo({ MyScene() })
+    val sceneContainer = sceneContainer()
+    sceneContainer.changeTo({ MyScene() })
 }
 
 class MyScene : Scene() {
-	override suspend fun SContainer.sceneMain() {
-		val minDegrees = (-16).degrees
-		val maxDegrees = (+16).degrees
+    override suspend fun SContainer.sceneMain() {
+        val minDegrees = (-16).degrees
+        val maxDegrees = (+16).degrees
 
-		val image = image(resourcesVfs["korge.png"].readBitmap()) {
-			rotation = maxDegrees
-			anchor(.5, .5)
-			scale(0.8)
-			position(256, 256)
-		}
+        val image = image(resourcesVfs["korge.png"].readBitmap()) {
+            rotation = maxDegrees
+            anchor(.5, .5)
+            scale(0.8)
+            position(256, 256)
+        }
 
-		while (true) {
-			image.tween(image::rotation[minDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
-			image.tween(image::rotation[maxDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
-		}
-	}
+        while (true) {
+            image.tween(image::rotation[minDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
+            image.tween(image::rotation[maxDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
+        }
+    }
 }
