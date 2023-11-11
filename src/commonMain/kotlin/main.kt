991f3f6566c49e61a3ea1741aebbe30f57141456
LostMekka <LostMekka@users.noreply.github.com>
2023-11-11 16:01:57 +0100

    add game resource store

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,18 +1,9 @@
-import de.lms.gj10.minesweeper.*
-import korlibs.datastructure.*
-import korlibs.image.bitmap.*
-import korlibs.time.*
+import de.lms.gj10.*
 import korlibs.korge.*
 import korlibs.korge.scene.*
-import korlibs.korge.tween.*
 import korlibs.korge.view.*
 import korlibs.image.color.*
-import korlibs.image.format.*
-import korlibs.image.tiles.*
-import korlibs.io.file.std.*
-import korlibs.korge.view.tiles.*
 import korlibs.math.geom.*
-import korlibs.math.interpolation.*
 
 suspend fun main() = Korge(windowSize = Size(512, 512), backgroundColor = Colors["#2b2b2b"]) {
     val sceneContainer = sceneContainer()
@@ -21,8 +12,7 @@ suspend fun main() = Korge(windowSize = Size(512, 512), backgroundColor = Colors
 
 class MyScene : Scene() {
     override suspend fun SContainer.sceneMain() {
-        val minDegrees = (-16).degrees
-        val maxDegrees = (+16).degrees
+        initializeGameResources() // must be the first thing here!
 
         val gridManager = GridManager(this)
         gridManager.initializeGrid()
